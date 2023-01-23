4c3f476a3d1b84b88be154fc1491de908fd71af4
Alhassan Dumbuya <alhassandumbuya@Alhassans-MacBook-Air.local>
2023-01-23 11:22:07 +0000

    appenchild the questions

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -6,7 +6,23 @@ if (isWin = false) {
     questions = isWin
 }
 
-/*
+//creating the questions useing appenchild elements
+const ptag1 = document.createElement("p");
+ptag1.textContent = "Which of this is an example of an element?";
+questiontitle.appendChild(ptag1)
 
+
+const ptag2 = document.createElement("p");
+ptag2.textContent = "Can you use CSS on javascript?";
+questiontitle.appendChild(ptag2)
+
+const ptag3 = document.createElement("p");
+ptag3.textContent = "This is the third example?";
+questiontitle.appendChild(ptag3)
+
+/*
+var tag = document.createElement(tagName);
+tag.textContent = "This was made via prompts. It's a " + tagName + ".";
+  document.body.appendChild(tag);
 */
 
